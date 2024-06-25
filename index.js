20eea4571d4d04d3b4ba522076e527bbc694fc40
Dave Abhishek <abhishekjdave0309@gmail.com>
2024-06-25 12:13:14 +0530

    add functionality to create tasks

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = 3000;
+const fs = require('fs')
 
 // Set EJS as view engine
 app.set('view engine', 'ejs');
@@ -17,7 +18,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Route handler for the root path
 app.get('/', (req, res) => {
-  res.render('index');
+    fs.readdir(`./files`, function(err, files){
+        console.log(files);
+        res.render('index', {files: files});
+    })
+});
+
+app.post('/create', function (req, res){
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.textarea, function(err){
+        res.redirect("/"); 
+    });
+    // console.log(req.body);
 });
 
 // Start the server
